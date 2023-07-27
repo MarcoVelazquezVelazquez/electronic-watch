6e2d071d95c3f0c4e8d227b049281e4a7d4d488b
Marco Antonio Velázquez Velázquez <velazquezv.marco@gmail.com>
2023-07-26 22:46:04 -0600

    Solved

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -7,6 +7,11 @@ public class ElectronicWatch {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
 
+        //int convertToHours = seconds / 3600;
+        int convertToHours = (seconds / 3600) % 24;
+        int convertToMinutes = (seconds % 3600) / 60;
+        int convertToSeconds = seconds % 60;
 
+        System.out.printf("%d:%02d:%02d", convertToHours, convertToMinutes, convertToSeconds);
     }
 }
